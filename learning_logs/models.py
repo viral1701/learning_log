84de3f1452a4ef275182d0aae1cd78c63ae725c4
Viral Patel <viral.patel@fourth.com>
2018-09-10 16:37:12 +0100

    commit

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    """ A topic the user is learning about """
+    text = models.CharField(max_length=200)
+    date_addedd = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """ Return a string representation of the model """
+        return self.text
+    
